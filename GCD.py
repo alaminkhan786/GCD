a4c839c80a3581e8664e84ebe80cf485da092300
MD AL AMIN KHAN <122734003+alaminkhan786@users.noreply.github.com>
2023-11-16 11:23:15 +0600

    first commit

---
+++ GCD.py
@@ -0,0 +1,11 @@
+def gcd(a, b):
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a % b)
+
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+result = gcd(num1, num2)
+print("The GCD of", num1, "and", num2, "is", result)
